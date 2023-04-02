b861996fc1602d8dc3fdc443ad141609d34e5b5d
Neiber Cardenas <rebienkrdns@gmail.com>
2023-04-02 08:45:27 -0500

    Docker install

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+apt-get remove docker docker-engine docker.io containerd runc
+
+apt-get update
+
+apt-get install ca-certificates curl gnupg
+
+mkdir -m 0755 -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
